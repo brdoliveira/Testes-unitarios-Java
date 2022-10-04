e707f35e07a0e61ce379ea258d0924f100c357ac
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-03 23:52:17 -0300

    FEAT(AssertTest): Criando arquivo para testar Asserts

--- src/test/java/br/ce/wcaquino/servicos/AssertTest.java
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -1,4 +1,44 @@
 package br.ce.wcaquino.servicos;
 
+import br.ce.wcaquino.entidades.Usuario;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AssertTest {
+    
+    @Test
+    public void Test(){
+        Assert.assertTrue(true);
+        Assert.assertFalse(false);
+
+        Assert.assertEquals("Erro de comparacao",1,1);
+        Assert.assertEquals(0.51,0.51,0.01); // DELTA = Margem de erro
+        Assert.assertEquals(Math.PI,3.14, 0.01);
+
+        int i = 5;
+        Integer i2 = 5;
+        // Auto-boxing e unboxing
+        Assert.assertEquals(Integer.valueOf(i),i2);
+        Assert.assertEquals(i,i2.intValue());
+
+        Assert.assertEquals("bola","bola"); // esperado e atual
+        Assert.assertTrue("bola".equalsIgnoreCase("Bola"));
+        Assert.assertTrue("bola".startsWith("bo"));
+
+        Usuario u1 = new Usuario("Usuario 1");
+        Usuario u2 = new Usuario("Usuario 1");
+        Usuario u3 = u2;
+        Usuario u4 = null;
+
+        Assert.assertEquals(u1,u2);
+
+        Assert.assertSame(u3,u2);
+
+        Assert.assertNull(u4);
+
+        Assert.assertNotEquals(u4,u2);
+        Assert.assertNotSame(u1,u2);
+        Assert.assertNotNull(u1);
+    }
+
 }
--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -5,14 +5,20 @@ import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.servicos.LocacaoService;
 import br.ce.wcaquino.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 public class LocacaoServiceTest {
+    @Rule // Cria uma pilha com os erros
+    public ErrorCollector error = new ErrorCollector(); // Notacao bem parecida do assert
+
     @Test
-    public void teste() {
+    public void testeLocacao() {
         //cenario
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Usuario 1");
@@ -22,8 +28,8 @@ public class LocacaoServiceTest {
         Locacao locacao = service.alugarFilme(usuario,filme);
 
         //validação
-        Assert.assertTrue(locacao.getValor() == 5.0);
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        error.checkThat(locacao.getValor(), CoreMatchers.is(CoreMatchers.equalTo(5.0))); // Adicionar import estático
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(),new Date()), CoreMatchers.is(true));
+        error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),CoreMatchers.is(true));
     }
 }

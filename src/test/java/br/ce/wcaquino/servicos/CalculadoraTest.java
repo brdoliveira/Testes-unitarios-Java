8cf653876dceadd0afea811a39a1a1f054efe677
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-12 23:51:48 -0300

    FEAT(CalculadoraTest): Adicionando novo teste

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -65,4 +65,14 @@ public class CalculadoraTest {
         // verificacao
         Assert.assertEquals(2,resultado);
     }
+
+    @Test
+    public void deveDividir(){
+        String a = "6";
+        String b = "3";
+
+        int resultado = calc.divide(a,b);
+
+        Assert.assertEquals(2,resultado);
+    }
 }

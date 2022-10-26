4ec57334dd13cb5bf450a23f9cc6be926d3fc912
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-25 23:43:05 -0300

    FEAT(LocacaoBuilder): Criando Builder para usuarios atrasados

--- src/test/java/br/ce/wcaquino/builders/LocacaoBuilder.java
+++ src/test/java/br/ce/wcaquino/builders/LocacaoBuilder.java
@@ -10,6 +10,7 @@ import br.ce.wcaquino.utils.DataUtils;
 
 import static br.ce.wcaquino.builders.FilmeBuilder.umFilme;
 import static br.ce.wcaquino.builders.UsuarioBuilder.umUsuario;
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
 
 
 public class LocacaoBuilder {
@@ -30,7 +31,7 @@ public class LocacaoBuilder {
         elemento.setUsuario(umUsuario().agora());
         elemento.setFilmes(Arrays.asList(umFilme().agora()));
         elemento.setDataLocacao(new Date());
-        elemento.setDataRetorno(DataUtils.obterDataComDiferencaDias(1));
+        elemento.setDataRetorno(obterDataComDiferencaDias(1));
         elemento.setValor(4.0);
     }
 
@@ -49,8 +50,14 @@ public class LocacaoBuilder {
         return this;
     }
 
-    public LocacaoBuilder comDataRetorno(Date param) {
+    public LocacaoBuilder comDataRetorno(Date param){
         elemento.setDataRetorno(param);
+        return this;
+    }
+
+    public LocacaoBuilder atrasada() {
+        elemento.setDataLocacao(obterDataComDiferencaDias(4));
+        elemento.setDataRetorno(obterDataComDiferencaDias(-2));
         return this;
     }
 

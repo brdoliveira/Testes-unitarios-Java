0c6fcea4236a06464651bb9d1118f184c671def0
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-25 23:44:28 -0300

    FEAT(CalculadoraMockTest): Criando teste para método de somar()

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,2 +1,15 @@
-package br.ce.wcaquino.servicos;public class CalculadoraMockTest {
+package br.ce.wcaquino.servicos;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class CalculadoraMockTest {
+
+    @Test
+    public void teste(){
+        Calculadora calc = Mockito.mock(Calculadora.class);
+        Mockito.when(calc.somar(Mockito.eq(1),Mockito.anyInt())).thenReturn(5);
+
+        //System.out.println(calc.somar(1,2)); // não sabe o que fazer, logo retorna zero.
+    }
 }

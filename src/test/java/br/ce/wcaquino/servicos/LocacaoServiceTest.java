3a5c977a4471f8c4bba2e8c306e67f0d14c68aa2
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-30 00:00:27 -0300

    FEAT(LocacaoServiceTest): Melhorando teste

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,5 +1,6 @@
 package br.ce.wcaquino.servicos;
 
+import br.ce.wcaquino.builders.LocacaoBuilder;
 import br.ce.wcaquino.daos.LocacaoDAO;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -11,10 +12,7 @@ import org.hamcrest.CoreMatchers;
 import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -193,5 +191,23 @@ public class LocacaoServiceTest {
 
         // acao
         service.alugarFilme(usuario,filmes);
+    }
+
+    @Test
+    public void deveProrrogarUmaLocacao(){
+        // cenario
+        Locacao locacao = LocacaoBuilder.umLocacao().agora();
+
+        // acao
+        service.prorrogarLocacao(locacao,3);
+
+        // verificacao
+        ArgumentCaptor<Locacao> argCapt = ArgumentCaptor.forClass(Locacao.class);
+        Mockito.verify(dao).salvar(argCapt.capture());
+        Locacao locacaoRetornada = argCapt.getValue();
+
+        error.checkThat(locacaoRetornada.getValor(),CoreMatchers.is(12.0));
+        error.checkThat(locacaoRetornada.getDataLocacao(),ehHoje());
+        error.checkThat(locacaoRetornada.getDataRetorno(),ehHojeComDiferencaDias(3));
     }
 }

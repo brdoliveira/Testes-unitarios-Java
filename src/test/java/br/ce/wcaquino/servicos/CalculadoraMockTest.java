571a5c71713ed9a964d9d1a1bf1d20112ba6dc8b
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-30 00:00:12 -0300

    FEAT(CalculadoraMockTest): Melhorando teste

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,7 +1,9 @@
 package br.ce.wcaquino.servicos;
 
+import br.ce.wcaquino.entidades.Locacao;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class CalculadoraMockTest {
@@ -9,9 +11,12 @@ public class CalculadoraMockTest {
     @Test
     public void teste(){
         Calculadora calc = Mockito.mock(Calculadora.class);
-        Mockito.when(calc.somar(Mockito.eq(1),Mockito.anyInt())).thenReturn(5);
+
+        ArgumentCaptor<Integer> argCapt = ArgumentCaptor.forClass(Integer.class);
+        Mockito.when(calc.somar(argCapt.capture(),argCapt.capture())).thenReturn(5);
 
         //System.out.println(calc.somar(1,2)); // não sabe o que fazer, logo retorna zero.
         Assert.assertEquals(5, calc.somar(1,100000));
+//        System.out.println(argCapt.getAllValues());
     }
 }

1ff8e2dcef1ef688399bfad577b115f41b830f48
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-10-30 23:43:53 -0300

    FEAT(CalculadoraMockTest): Criandoteste unitario com Mock e Spy

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -2,11 +2,41 @@ package br.ce.wcaquino.servicos;
 
 import br.ce.wcaquino.entidades.Locacao;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
+import org.mockito.*;
 
 public class CalculadoraMockTest {
+
+    @Mock // Caso ele não saiba o que fazer retorna 0
+    private Calculadora calcMock;
+
+    @Spy // Caso ele não saiba o que fazer roda o método
+    private Calculadora calcSpy;
+
+    @Spy
+    private  EmailService email;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void devoMostrarDiferencaEntreMockSpy(){
+        Mockito.when(calcMock.somar(1,2)).thenReturn(5);
+//        Mockito.when(calcSpy.somar(1,2)).thenReturn(5);
+        Mockito.doReturn(5).when(calcSpy).somar(1,2);
+        Mockito.doNothing().when(calcSpy).imprime();
+
+        System.out.println("Mock: " + calcMock.somar(1,2));
+        System.out.println("Spy: " + calcSpy.somar(1,2));
+
+        System.out.println("Mock");
+        calcMock.imprime();
+        System.out.println("Spy");
+        calcSpy.imprime();
+    }
 
     @Test
     public void teste(){

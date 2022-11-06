cf86d1a60d53606bd7daf8adceca196887531fc2
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2022-11-05 23:18:49 -0300

    FEAT(LocacaoServiceTest): Testando metodos privados diretamente

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -18,6 +18,7 @@ import org.mockito.*;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import java.util.Arrays;
 import java.util.Calendar;
@@ -250,5 +251,18 @@ public class LocacaoServiceTest {
         // verificacao
         Assert.assertThat(locacao.getValor(),CoreMatchers.is(1.0));
         PowerMockito.verifyPrivate(service).invoke("calcularValorLocacao",filmes);
+    }
+
+    @Test
+    public void deveCalcularValorLocacao() throws Exception{
+        // cenario
+        List<Filme> filmes = Arrays.asList(umFilme().agora());
+
+        // acao
+        // (Double) = cast
+        Double valor = (Double) Whitebox.invokeMethod(service,"calcularValorLocacao", filmes);
+
+        // verificacao
+        Assert.assertThat(valor,CoreMatchers.is(4.0));
     }
 }
